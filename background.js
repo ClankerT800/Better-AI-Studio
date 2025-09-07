c6b7ef9001399ed8a2643b572cfd808271f6898c
ClankerT800 <raslan86564@gmail.com>
2025-09-07 12:04:55 +0200

    commit

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-    if (details.url.startsWith('https://aistudio.google.com/prompts/new_chat') && !details.url.includes('model=gemini-1.5-flash-preview')) {
+    if (details.url.startsWith('https://aistudio.google.com')) {
         chrome.scripting.executeScript({
             target: { tabId: details.tabId },
             files: ['content.js']
